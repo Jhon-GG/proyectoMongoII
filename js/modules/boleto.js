8378b03ae75fff38b56e2addefad65d99519a50b
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-28 23:56:36 -0500

    feat: :construction: Avance en la primer parte del caso 2
    Se realizaron avances en la primer parte del caso 2

--- js/modules/boleto.js
+++ js/modules/boleto.js
@@ -2,33 +2,85 @@ import { connect } from "../../helpers/db/connect.js"
 import { ObjectId } from "mongodb";
 
 
-// 1. Seleccion de peliculas
+// 2. Compra de boletos
 
-export class pelicula extends connect {
-    static instancePelicula;
+export class boleto extends connect {
+    static instanceBoleto;
     db;
     collection;
 
     constructor() {
-        if (pelicula.instancePelicula) {
-            return pelicula.instancePelicula;
+        if (boleto.instanceBoleto) {
+            return boleto.instanceBoleto;
         }
         super();
         this.db = this.conexion.db(this.getDbName);
-        this.collection = this.db.collection('pelicula');
-        pelicula.instancePelicula = this;
+        this.collection = this.db.collection('boleto');
+        boleto.instanceBoleto = this;
     }
     destructor() {
-        pelicula.instancePelicula = undefined;
+        boleto.instanceBoleto = undefined;
         connect.instanceConnect = undefined;
     }
 
-    async getTest() {
+    async crearBoleto(nuevoBoleto) {
         await this.conexion.connect();
-        const collection = this.db.collection('test');
-        const data = await collection.find({}).toArray();
+
+        // Verificar si ya existe un boleto con el mismo id
+        const boletoExistentePorId = await this.collection.findOne({ id: nuevoBoleto.id });
+
+        if (boletoExistentePorId) {
+            await this.conexion.close();
+            throw new Error("Ya existe un boleto con el mismo ID.");
+        }
+
+        // Verificar si ya existe un boleto con los mismos datos 
+        const boletoExistente = await this.collection.findOne({
+            id_pelicula: nuevoBoleto.id_pelicula,
+            id_horario_funcion: nuevoBoleto.id_horario_funcion,
+            id_usuario: nuevoBoleto.id_usuario,
+            id_reserva: nuevoBoleto.id_reserva,
+            asiento: nuevoBoleto.asiento,
+            tipo_compra: nuevoBoleto.tipo_compra,
+            fecha_compra: nuevoBoleto.fecha_compra,
+            metodo_pago: nuevoBoleto.metodo_pago,
+            estado_compra: nuevoBoleto.estado_compra
+        });
+
+        if (boletoExistente) {
+            await this.conexion.close();
+            throw new Error("Ya existe un boleto con los mismos datos.");
+        }
+
+        // Obtener detalles de la función
+        const horarioColeccion = this.db.collection('horario_funcion');
+        const horario = await horarioColeccion.findOne({ id: nuevoBoleto.id_horario_funcion });
+
+        if (!horario) {
+            await this.conexion.close();
+            throw new Error("Horario de función no encontrado.");
+        }
+
+        // Añadir fecha y hora de la función al boleto
+        nuevoBoleto.fecha_funcion = horario.fecha_funcion;
+        nuevoBoleto.hora_funcion = horario.hora_funcion;
+
+        // Verificar tipo de usuario
+        const usuarioColeccion = this.db.collection('tarjeta_vip');
+        const usuarioVIP = await usuarioColeccion.findOne({ id_usuario: nuevoBoleto.id_usuario });
+
+        if (usuarioVIP && usuarioVIP.estado === 'activa') {
+            const descuento = usuarioVIP.descuento / 100;
+            nuevoBoleto.total = horario.precio * (1 - descuento);
+        } else {
+            nuevoBoleto.total = horario.precio;
+        }
+
+        // Insertar el boleto en la colección
+        await this.collection.insertOne(nuevoBoleto);
+
         await this.conexion.close();
-        return data;
+        return nuevoBoleto;
     }
 
 }
--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 import {pelicula} from './js/modules/pelicula.js'
+import {boleto} from './js/modules/boleto.js'
 
 
 // 1. Seleccion de peliculas
 
 
 // Permitir la consulta de todas las películas disponibles en el catálogo, con detalles como título, género, duración y horarios de proyección.
-// let objPelicula = new pelicula();
 
-// console.log(`Peliculas disponibles: `, await objPelicula.getPeliculas());
+    // let objPelicula = new pelicula();
 
-// objPelicula.destructor();
+    // console.log(`Peliculas disponibles: `, await objPelicula.getPeliculas());
+
+    // objPelicula.destructor();
+
 
 // Permitir la consulta de información detallada sobre una película específica, incluyendo sinopsis.
 
-const idPeliculaById = 2; 
+    // const idPeliculaById = 2; 
 
-    let objPelicula = new pelicula();
+    //     let objPelicula = new pelicula();
 
-    console.log(`Información de la película con ID ${idPeliculaById}: `, await objPelicula.getPeliculaById(idPeliculaById));
+    //     console.log(`Información de la película con ID ${idPeliculaById}: `, await objPelicula.getPeliculaById(idPeliculaById));
 
-    objPelicula.destructor();
+    //     objPelicula.destructor();
+
+
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+
+// 2. Compra de boletos
+
+const nuevoBoleto = {
+    id: 9,
+    id_pelicula: 14,
+    id_horario_funcion: 17,
+    id_usuario: 2,
+    id_reserva: null,
+    asiento: "A1",
+    tipo_compra: "virtual",
+    fecha_compra: "2024-07-21",
+    metodo_pago: "tarjeta de crédito",
+    estado_compra: "realizado",
+    total: 0 
+  };
+  
+      let objBoleto = new boleto();
+  
+          const boletoCreado = await objBoleto.crearBoleto(nuevoBoleto);
+          console.log(`Boleto creado: `, boletoCreado);
+
+  
+      objBoleto.destructor();
