f9f81f29a3b3cddbfd72620e5c73e867a430dfd6
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-07-28 20:02:08 -0500

    feat: :construction: Finalizacion de la primer parte del caso 1
    Se generó la finalizacion de la primera parte del caso 1

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import {pelicula} from './js/modules/pelicula.js'
 
+
+// 1. Seleccion de peliculas
+
+
+// Permitir la consulta de todas las películas disponibles en el catálogo, con detalles como título, género, duración y horarios de proyección.
 let objPelicula = new pelicula();
 
 console.log(`Peliculas disponibles: `, await objPelicula.getPeliculas());
 
 objPelicula.destructor();
+
